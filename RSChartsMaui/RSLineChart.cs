3a3e193f4d62b9e544cbd86861c2c1f08c6f2b44
rcvetkoski <50323773+rcvetkoski@users.noreply.github.com>
2025-01-15 14:15:30 +0100

    ...

--- RSChartsMaui/RSLineChart.cs
+++ RSChartsMaui/RSLineChart.cs
@@ -11,24 +11,22 @@
 
         public void Draw(ICanvas canvas, RectF dirtyRect)
         {
-            // Define margins using Thickness
-            Thickness margin = new Thickness(40, 10, 10, 40);
-
-            // Calculate dimensions
+            // Define margins and dimensions
+            float margin = 40;
             float width = (float)dirtyRect.Width;
             float height = (float)dirtyRect.Height;
-            float chartWidth = width - (float)(margin.Left + margin.Right);
-            float chartHeight = height - (float)(margin.Top + margin.Bottom);
+            float chartWidth = width - 2 * margin;
+            float chartHeight = height - 2 * margin;
 
             // Set up axis lines
-            canvas.StrokeColor = Colors.LightGray;
+            canvas.StrokeColor = chart.AxisLineColor;
             canvas.StrokeSize = 1;
 
             // Draw X-axis
-            canvas.DrawLine((float)margin.Left, height - (float)margin.Bottom, width - (float)margin.Right, height - (float)margin.Bottom);
+            canvas.DrawLine(margin, height - margin, width - margin, height - margin);
 
             // Draw Y-axis
-            canvas.DrawLine((float)margin.Left, (float)margin.Top, (float)margin.Left, height - (float)margin.Bottom);
+            canvas.DrawLine(margin, margin, margin, height - margin);
 
             // Get data points
             var dataPoints = chart.ChartData;
@@ -41,31 +39,76 @@
             float maxDataValue = chart.MaxDataValue;
             float yScale = chartHeight / maxDataValue;
 
-            // Draw data points as a line chart
-            canvas.StrokeColor = Colors.Blue;
-            canvas.StrokeSize = 3;
-            for (int i = 0; i < dataCount - 1; i++)
+            if (chart.IsCurved)
             {
-                float x1 = (float)margin.Left + i * xInterval;
-                float y1 = height - (float)margin.Bottom - (dataPoints[i] * yScale);
-                float x2 = (float)margin.Left + (i + 1) * xInterval;
-                float y2 = height - (float)margin.Bottom - (dataPoints[i + 1] * yScale);
+                // Draw smooth curves
+                var path = new PathF();
 
-                canvas.DrawLine(x1, y1, x2, y2);
+                // Start the path at the first data point
+                float startX = margin;
+                float startY = height - margin - (dataPoints[0] * yScale);
+                path.MoveTo(startX, startY);
+
+                for (int i = 0; i < dataCount - 1; i++)
+                {
+                    // Get the current, next, and neighboring points
+                    float x0 = i == 0 ? margin : margin + (i - 1) * xInterval;
+                    float y0 = i == 0 ? height - margin - (dataPoints[0] * yScale) : height - margin - (dataPoints[i - 1] * yScale);
+
+                    float x1 = margin + i * xInterval;
+                    float y1 = height - margin - (dataPoints[i] * yScale);
+
+                    float x2 = margin + (i + 1) * xInterval;
+                    float y2 = height - margin - (dataPoints[i + 1] * yScale);
+
+                    float x3 = i + 2 < dataCount ? margin + (i + 2) * xInterval : x2;
+                    float y3 = i + 2 < dataCount ? height - margin - (dataPoints[i + 2] * yScale) : y2;
+
+                    // Calculate control points
+                    float controlX1 = x1 + (x2 - x0) / 6f;
+                    float controlY1 = y1 + (y2 - y0) / 6f;
+
+                    float controlX2 = x2 - (x3 - x1) / 6f;
+                    float controlY2 = y2 - (y3 - y1) / 6f;
+
+                    // Add a cubic curve segment to the path
+                    path.CurveTo(controlX1, controlY1, controlX2, controlY2, x2, y2);
+                }
+
+                // Draw the smooth path
+                canvas.StrokeColor = chart.LineColor;
+                canvas.StrokeSize = 3;
+                canvas.DrawPath(path);
+            }
+            else
+            {
+                // Draw straight lines
+                canvas.StrokeColor = chart.LineColor;
+                canvas.StrokeSize = 3;
+
+                for (int i = 0; i < dataCount - 1; i++)
+                {
+                    float x1 = margin + i * xInterval;
+                    float y1 = height - margin - (dataPoints[i] * yScale);
+                    float x2 = margin + (i + 1) * xInterval;
+                    float y2 = height - margin - (dataPoints[i + 1] * yScale);
+
+                    canvas.DrawLine(x1, y1, x2, y2);
+                }
             }
 
             // Add axis labels and indicators
             canvas.FontSize = 12;
-            canvas.FontColor = Colors.Gray;
-            canvas.StrokeColor = Colors.LightGray;
+            canvas.FontColor = chart.AxisLabelColor;
+            canvas.StrokeColor = chart.AxisLineColor;
             canvas.StrokeSize = 1;
 
             // X-axis labels and indicators
             int labelInterval = Math.Max(1, dataCount / 12); // Show ~12 labels
             for (int i = 0; i < dataCount; i += labelInterval)
             {
-                float x = (float)margin.Left + i * xInterval;
-                float y = height - (float)margin.Bottom;
+                float x = margin + i * xInterval;
+                float y = height - margin;
 
                 canvas.DrawLine(x, y, x, y + 5);
                 canvas.DrawString((i + 1).ToString(), x, y + 20, HorizontalAlignment.Center);
@@ -74,8 +117,8 @@
             // Y-axis labels and indicators
             for (int i = 0; i <= maxDataValue; i += 10)
             {
-                float x = (float)margin.Left;
-                float y = height - (float)margin.Bottom - (i * yScale);
+                float x = margin;
+                float y = height - margin - (i * yScale);
 
                 // Draw indicator
                 canvas.DrawLine(x - 5, y, x, y);
@@ -113,6 +156,46 @@
                 ((RSLineChart)bindable).Invalidate();
             });
 
+        public static readonly BindableProperty IsCurvedProperty = BindableProperty.Create(
+            nameof(IsCurved),
+            typeof(bool),
+            typeof(RSLineChart),
+            false,
+            propertyChanged: (bindable, oldValue, newValue) =>
+            {
+                ((RSLineChart)bindable).Invalidate();
+            });
+
+        public static readonly BindableProperty LineColorProperty = BindableProperty.Create(
+            nameof(LineColor),
+            typeof(Color),
+            typeof(RSLineChart),
+            Colors.Blue,
+            propertyChanged: (bindable, oldValue, newValue) =>
+            {
+                ((RSLineChart)bindable).Invalidate();
+            });
+
+        public static readonly BindableProperty AxisLineColorProperty = BindableProperty.Create(
+            nameof(AxisLineColor),
+            typeof(Color),
+            typeof(RSLineChart),
+            Colors.LightGray,
+            propertyChanged: (bindable, oldValue, newValue) =>
+            {
+                ((RSLineChart)bindable).Invalidate();
+            });
+
+        public static readonly BindableProperty AxisLabelColorProperty = BindableProperty.Create(
+            nameof(AxisLabelColor),
+            typeof(Color),
+            typeof(RSLineChart),
+            Colors.Gray,
+            propertyChanged: (bindable, oldValue, newValue) =>
+            {
+                ((RSLineChart)bindable).Invalidate();
+            });
+
         public IList<float> ChartData
         {
             get => (IList<float>)GetValue(ChartDataProperty);
@@ -123,6 +206,30 @@
         {
             get => (float)GetValue(MaxDataValueProperty);
             set => SetValue(MaxDataValueProperty, value);
+        }
+
+        public bool IsCurved
+        {
+            get => (bool)GetValue(IsCurvedProperty);
+            set => SetValue(IsCurvedProperty, value);
+        }
+
+        public Color LineColor
+        {
+            get => (Color)GetValue(LineColorProperty);
+            set => SetValue(LineColorProperty, value);
+        }
+
+        public Color AxisLineColor
+        {
+            get => (Color)GetValue(AxisLineColorProperty);
+            set => SetValue(AxisLineColorProperty, value);
+        }
+
+        public Color AxisLabelColor
+        {
+            get => (Color)GetValue(AxisLabelColorProperty);
+            set => SetValue(AxisLabelColorProperty, value);
         }
     }
 }
